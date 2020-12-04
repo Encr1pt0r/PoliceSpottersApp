b97a536536fec773fdbc619e7e814db55fb2b3a5
Encr1pt0r <presidor16@gmail.com>
2020-12-04 22:38:11 +0000

    Initial commit

---
+++ app/src/main/java/com/Encr1pt0r/kevin_police_spotters/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Encr1pt0r.kevin_police_spotters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/Encr1pt0r/kevin_police_spotters/NewEntryActivity.java
@@ -0,0 +1,17 @@
+package com.Encr1pt0r.kevin_police_spotters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_entry);
+
+        // https://www.tutlane.com/tutorial/android/android-spinner-dropdown-list-with-examples
+        // something I found to help with the result stuff
+    }
+}
