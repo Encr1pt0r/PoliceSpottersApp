cf90c45533762cf49b704063c3f1878d2e7d38fa
Encr1pt0r <presidor16@gmail.com>
2020-12-08 15:58:04 +0000

    Added view model

---
+++ app/src/main/java/com/Encr1pt0r/kevin_police_spotters/model/viewmodel/SpotCheckVM.java
@@ -0,0 +1,31 @@
+package com.Encr1pt0r.kevin_police_spotters.model.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.Encr1pt0r.kevin_police_spotters.model.SpotCheck;
+import com.Encr1pt0r.kevin_police_spotters.model.SpotCheckRepo;
+
+import java.util.List;
+
+public class SpotCheckVM extends AndroidViewModel {
+
+    private SpotCheckRepo repo;
+    private LiveData<List<SpotCheck>> allSpotChecks;
+
+    public SpotCheckVM(Application application) {
+        super(application);
+        repo = new SpotCheckRepo(application);
+        allSpotChecks = repo.getAllSpotChecks();
+    }
+
+    public LiveData<List<SpotCheck>> getAllSpotChecks() {
+        return allSpotChecks;
+    }
+
+    public void insert(SpotCheck spotCheck) {
+        repo.insertSpotCheck(spotCheck);
+    }
+}
