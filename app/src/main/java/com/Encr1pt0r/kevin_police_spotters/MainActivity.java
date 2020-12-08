82d82ea03b413757ed028b286893f5be48fbf0be
Encr1pt0r <presidor16@gmail.com>
2020-12-08 20:45:26 +0000

    Updated UI button links to add spot check screen

--- app/src/main/java/com/Encr1pt0r/kevin_police_spotters/MainActivity.java
+++ app/src/main/java/com/Encr1pt0r/kevin_police_spotters/MainActivity.java
@@ -2,7 +2,9 @@ package com.Encr1pt0r.kevin_police_spotters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addSpotCheck(View view) {
+        Intent addSpotCheckIntent = new Intent(this, NewEntryActivity.class);
+        startActivity(addSpotCheckIntent);
     }
 }
--- app/src/main/java/com/Encr1pt0r/kevin_police_spotters/NewEntryActivity.java
+++ app/src/main/java/com/Encr1pt0r/kevin_police_spotters/NewEntryActivity.java
@@ -3,6 +3,8 @@ package com.Encr1pt0r.kevin_police_spotters;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewEntryActivity extends AppCompatActivity {
 
@@ -13,5 +15,10 @@ public class NewEntryActivity extends AppCompatActivity {
 
         // https://www.tutlane.com/tutorial/android/android-spinner-dropdown-list-with-examples
         // something I found to help with the result stuff
+
+        Spinner spinner = (Spinner) findViewById(R.id.result_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.results_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
