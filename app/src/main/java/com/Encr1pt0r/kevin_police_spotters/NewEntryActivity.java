e09428d7a5bf354dc2327b00b217c5effe8c970a
Encr1pt0r <presidor16@gmail.com>
2020-12-09 15:42:44 +0000

    Implemented reply intent with user input

--- app/src/main/java/com/Encr1pt0r/kevin_police_spotters/NewEntryActivity.java
+++ app/src/main/java/com/Encr1pt0r/kevin_police_spotters/NewEntryActivity.java
@@ -2,8 +2,13 @@ package com.Encr1pt0r.kevin_police_spotters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class NewEntryActivity extends AppCompatActivity {
@@ -17,6 +22,7 @@ public class NewEntryActivity extends AppCompatActivity {
          * @TODO - finish interactions with the spinner
          * https://www.tutlane.com/tutorial/android/android-spinner-dropdown-list-with-examples
          * something I found to help with the result stuff
+         * https://developer.android.com/guide/topics/ui/controls/spinner
          * Just for you now to follow the second half of this stuff
          */
 
@@ -25,5 +31,44 @@ public class NewEntryActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.results_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+    }
+
+//    @Override
+//    public void onItemSelected(AdapterView<> parent, View view, int pos, long id) {
+//
+//    }
+
+    // Generate data for return to MainActivity
+    public void saveNewSpotCheck(View view) {
+        // Find components
+        EditText locationET = findViewById(R.id.editTextLocation);
+        EditText carRegET = findViewById(R.id.editTextCarRegNo);
+        EditText carModelET = findViewById(R.id.editTextCarModel);
+        Spinner resultSpinner = (Spinner) findViewById(R.id.result_spinner);
+        EditText notesET = findViewById(R.id.editTextNotes);
+
+        // Generate Strings
+        String location = locationET.getText().toString();
+        String carReg = carRegET.getText().toString();
+        String carModel = carModelET.getText().toString();
+        String result = resultSpinner.getSelectedItem().toString();
+        String notes = notesET.getText().toString();
+
+        // Debugging
+        Log.i("debug", location);
+        Log.i("debug", carReg);
+        Log.i("debug", carModel);
+        Log.i("debug", result);
+        Log.i("debug", notes);
+
+        // Add extras and finish Intent
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra("location", location);
+        replyIntent.putExtra("carReg", carReg);
+        replyIntent.putExtra("carModel", carModel);
+        replyIntent.putExtra("result", result);
+        replyIntent.putExtra("notes", notes);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
